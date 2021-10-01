31b2cc86e5a1a998be2ca5206908bbe1b5da6b92
baggioko1u <baggioko1u@iutm-inf-f36-07.iutmetz.site.univ-lorraine.fr>
2021-10-01 11:59:20 +0200

    reste que le SQL Abonnement et sa date de m****

--- src/dao/listememoire/ListeMemoireAbonnementDAO.java
+++ src/dao/listememoire/ListeMemoireAbonnementDAO.java
@@ -1,5 +1,6 @@
 package dao.listememoire;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import dao.AbonnementDAO;
@@ -23,9 +24,12 @@ public class ListeMemoireAbonnementDAO implements AbonnementDAO {
 
 	        this.donnees = new ArrayList<Abonnement>();
 
-	        
-	        this.donnees.add(new Abonnement (8,"04/04/2020", "04/04/2021",9,5));
-	        this.donnees.add(new Abonnement (7,"25/01/2019", "25/01/2020",15,27));
+	        LocalDate d1 = LocalDate.parse("04/04/2020");
+	        LocalDate d2 = LocalDate.parse("04/04/2021");
+	        LocalDate d3 = LocalDate.parse("25/07/2019");
+	        LocalDate d4 = LocalDate.parse("25/07/2020");
+	        this.donnees.add(new Abonnement (8,d1,d2,9,5));
+	        this.donnees.add(new Abonnement (7,d3,d4,15,27));
 	    }
 	    
 
@@ -76,8 +80,10 @@ public class ListeMemoireAbonnementDAO implements AbonnementDAO {
 
 	    @Override
 	    public Abonnement getById(int id) {
-	        // Ne fonctionne que si l'objet m�tier est bien fait...
-	        int idx = this.donnees.indexOf(new Abonnement(id, "test", "test",id,id));
+	        
+	    	LocalDate d1 = LocalDate.parse("04/04/2020");
+	    	LocalDate d2 = LocalDate.parse("04/04/2021");
+	        int idx = this.donnees.indexOf(new Abonnement(id,d1,d2,0,0));
 	        if (idx == -1) {
 	            throw new IllegalArgumentException("Aucun objet ne poss�de cet identifiant");
 	        } else {
--- src/dao/mysql/MySQLAbonnementDAO.java
+++ src/dao/mysql/MySQLAbonnementDAO.java
@@ -3,18 +3,17 @@ package dao.mysql;
 import java.util.ArrayList;
 
 import java.sql.*;
+import java.time.format.DateTimeFormatter;
+
 import connexion.Connexion;
 import dao.AbonnementDAO;
 import modele.Abonnement;
-import modele.Abonnement;
+import modele.Periodicite;
 
 
 public class MySQLAbonnementDAO implements AbonnementDAO{
 
 	 private static MySQLAbonnementDAO instance;
-
-	  
-
 
 	    public static MySQLAbonnementDAO getInstance() {
 
@@ -30,21 +29,24 @@ public class MySQLAbonnementDAO implements AbonnementDAO{
 	    @Override
 		public boolean create(Abonnement objet) {
 			
-			int nbLigne = 0; 
+	    	int nbLigne = 0; 
 			
 			try {
 				Connexion c = new Connexion();
 				Connection laConnexion = c.creeConnexion();
 				PreparedStatement requete = 
 				
-				laConnexion.prepareStatement("insert into Abonnement (date_debut) values(?)", Statement.RETURN_GENERATED_KEYS);
-				requete.setString(1, objet.get());
+				laConnexion.prepareStatement("insert into Abonnement (date_debut, date_fin, id_client, id_revue) values(?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+				requete.setString(1, (String) objet.getDate_debut());
+				requete.setString(2, (String) objet.getDate_fin());
+				requete.setInt(3, objet.getId_client());
+				requete.setInt(4, objet.getId_revue());
 				
 				nbLigne = requete.executeUpdate();
 				
 				ResultSet res = requete.getGeneratedKeys();
 				if ( res.next() ) {
-					objet.setId_revue(res.getInt(1));
+					objet.setId_abonnement(res.getInt(1));
 				}
 				
 		} catch (SQLException sqle) {
@@ -64,9 +66,13 @@ public class MySQLAbonnementDAO implements AbonnementDAO{
 				Connection laConnexion = c.creeConnexion();
 				PreparedStatement requete = 
 				
-				laConnexion.prepareStatement("update Abonnement set Titre =? where id_revue =?");
-				requete.setString(1, objet.getTitre());
-				requete.setInt(2, objet.getId_revue());
+				laConnexion.prepareStatement("update Abonnement set date_debut =?, date_fin =?, id_client =?, id_revue =? where id_abonnement =?");
+				DateTimeFormatter formatage = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+				requete.setDate(1, Date.valueOf(formatage.format(objet.getDate_debut())));
+				requete.setDate(1, Date.valueOf(formatage.format(objet.getDate_fin())));
+				requete.setInt(3, objet.getId_client());
+				requete.setInt(4, objet.getId_revue());
+				requete.setInt(5, objet.getId_abonnement());
 				
 				nbLigne = requete.executeUpdate();
 				
@@ -87,8 +93,8 @@ public class MySQLAbonnementDAO implements AbonnementDAO{
 				Connection laConnexion = c.creeConnexion();
 				PreparedStatement requete = 
 				
-				laConnexion.prepareStatement("delete from Abonnement where id_revue=?");
-				requete.setInt(1, objet.getId_periodicite());
+				laConnexion.prepareStatement("delete from Abonnement where id_abonnement=?");
+				requete.setInt(1, objet.getId_abonnement());
 				
 				nbLigne = requete.executeUpdate();
 				
@@ -102,39 +108,39 @@ public class MySQLAbonnementDAO implements AbonnementDAO{
 		@Override
 		public Abonnement getById(int id) {
 			
-			Abonnement r = null;
+			Abonnement a = null;
 			
 			try {
 				Connexion c = new Connexion();
 				Connection laConnexion = c.creeConnexion();
 				PreparedStatement requete = 
 				
-				laConnexion.prepareStatement("select * from Abonnement where id_revue=?");
+				laConnexion.prepareStatement("select * from Abonnement where id_abonnement=?");
 				requete.setInt(1, id);
 				
 				ResultSet resultSet = requete.executeQuery();
 
 			if (resultSet.next()) {
-				r = new Abonnement();
-				r.setId_revue(resultSet.getInt("id_revue"));
-				r.setTitre(resultSet.getString("titre"));
-				r.setDescription(resultSet.getString("description"));
-				r.setTarif_numero(resultSet.getFloat("tarif_numero"));
-				r.setVisuel(resultSet.getString("visuel"));
-				r.setId_periodicite(resultSet.getInt("id_periodicite"));
+				a = new Abonnement();
+				DateTimeFormatter formatage = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+				a.setId_abonnement(resultSet.getInt("id_abonnement"));
+				a.setDate_debut(Date.valueOf(formatage.format(resultSet.getDate("date_deb"))));
+				a.setDate_fin(resultSet.getDate("date_fin"));
+				a.setId_client(resultSet.getInt("id_client"));
+				a.setId_revue(resultSet.getInt("id_revue"));
 			}
 				
 		} catch (SQLException sqle) {
 				System.out.println("Pb dans select " + sqle.getMessage());
 				}
 			
-			return r;
+			return a;
 		}
 
 		@Override
 		public ArrayList<Abonnement> findAll() {
 			
-			ArrayList<Abonnement> rList = new ArrayList<Abonnement>();
+			ArrayList<Abonnement> aList = new ArrayList<Abonnement>();
 			
 			try {
 				Connexion c = new Connexion();
@@ -146,16 +152,17 @@ public class MySQLAbonnementDAO implements AbonnementDAO{
 				ResultSet resultSet = requete.executeQuery();
 
 			while(resultSet.next()) {
-				rList.add(new Abonnement(resultSet.getInt("id_revue")
-						,resultSet.getString("titre"),resultSet.getString("description"),resultSet.getFloat("tarif_numero"),resultSet.getString("visuel"),resultSet.getInt("id_periodicite")));
+				aList.add(new Abonnement(resultSet.getInt("id_abonnement"),
+						resultSet.getString("date_debut"),
+						resultSet.getString("date_fin"),
+						));
 			}
 				
 		} catch (SQLException sqle) {
 				System.out.println("Pb dans select " + sqle.getMessage());
 				}
 			
-			return rList;
-			
+			return pList;
 		}
 }
 	   
